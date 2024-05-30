f9df46dd4831271448bdbdf79e0d508e6cbb026f
somesh4747 <somesh47.karmakar@gmail.com>
2024-05-30 16:38:09 +0530

    domain added
    send mail to anyone, .env.example updated

--- lib/mail.ts
+++ lib/mail.ts
@@ -9,7 +9,7 @@ const domain = process.env.NEXT_PUBLIC_DOMAIN
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/new-verification?token=${token}`
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'auth@someshdev.in',
         to: email,
         subject: 'confirm your email',
         html: `<p>click <a href=${confirmLink}>here</a></p>`,
@@ -19,7 +19,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordResetMail = async (email: string, token: string) => {
     const confirmLink = `${domain}/reset-password?token=${token}`
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'auth@someshdev.in',
         to: email,
         subject: 'Reset your password',
         html: `<p>click <a href=${confirmLink}>here</a> to reset the password</p>`,
@@ -27,7 +27,7 @@ export const sendPasswordResetMail = async (email: string, token: string) => {
 }
 export const sendTwoFactorMail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'auth@someshdev.in',
         to: email,
         subject: '2FA Mail',
         react: TwoFactorEmailTemplate({ verificationCode: token, time: '5' }),
